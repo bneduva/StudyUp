06d65a3c9db054053470670604d22aafd0c8b1bc
Benjamen Neduva <bneduva@ucdavis.edu>
2019-02-11 15:54:45 -0800

    Unit Tests Written

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -3,7 +3,9 @@ package edu.studyup.serviceImpl;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
@@ -76,4 +78,164 @@ class EventServiceImplTest {
 		  });
 	}
 	
+	@Test
+	void testUpdateEventName_LongEventName_BadCase(){
+		int eventID = 1;
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.updateEventName(eventID, "Renamed Event 1 - Long Name");
+		});
+	}
+
+	@Test
+	void testUpdateEventName_MaxLengthName_GoodCase() throws StudyUpException {
+		int eventID = 1;
+		eventServiceImpl.updateEventName(eventID, "Renamed Event 1 long");
+		assertEquals("Renamed Event 1 long", DataStorage.eventData.get(eventID).getName());
+	}
+	
+	@Test
+	void testGetActiveEvents_FutureEventDate_GoodCase(){
+		int eventID = 1;
+		Date futureDate = new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime();
+		DataStorage.eventData.get(eventID).setDate(futureDate);
+		
+		List<Event> result = eventServiceImpl.getActiveEvents();
+		
+		assertEquals(eventID, result.get(0).getEventID());
+	}
+	
+	@Test
+	void testGetActiveEvents_PastEventDate_BadCase(){
+		int eventID = 1;
+		Date futureDate = new GregorianCalendar(2000, Calendar.JANUARY, 1).getTime();
+		DataStorage.eventData.get(eventID).setDate(futureDate);
+		
+		List<Event> result = eventServiceImpl.getActiveEvents();
+		
+		assertEquals(0, result.size());
+	}
+	
+	@Test
+	void testGetPastEvents_PastEventDate_GoodCase(){
+		int eventID = 1;
+		Date pastDate = new GregorianCalendar(2000, Calendar.JANUARY, 1).getTime();
+		DataStorage.eventData.get(eventID).setDate(pastDate);
+		
+		List<Event> result = eventServiceImpl.getPastEvents();
+		assertEquals(eventID, result.get(0).getEventID());
+	}
+	
+	@Test
+	void testGetPastEvents_FutureEventDate_BadCase(){
+		int eventID = 1;
+		Date futureDate = new GregorianCalendar(2020, Calendar.JANUARY, 1).getTime();
+		DataStorage.eventData.get(eventID).setDate(futureDate);
+		
+		List<Event> result = eventServiceImpl.getPastEvents();
+		
+		assertEquals(0, result.size());
+	}
+	
+	@Test
+	void testAddStudentToEvent_StudentInEvent_GoodCase() throws StudyUpException {
+		int eventID = 1;
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(2);
+		
+		List<Student> expected = new ArrayList<>();
+		expected.add(DataStorage.eventData.get(eventID).getStudents().get(0));
+		expected.add(student);
+		
+		Event resultEvent = eventServiceImpl.addStudentToEvent(student, eventID);
+		
+		List<Student> resultStudents = resultEvent.getStudents();
+		
+		assertEquals(expected, resultStudents);
+	}
+	
+	@Test
+	void testAddStudentToEvent_NoStudentsInEvent_GoodCase() throws StudyUpException {
+		int eventID = 1;
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(2);
+		
+		List<Student> expected = new ArrayList<>();
+		expected.add(student);
+
+		DataStorage.eventData.get(eventID).setStudents(null);
+		
+		Event resultEvent = eventServiceImpl.addStudentToEvent(student, eventID);
+		
+		List<Student> resultStudents = resultEvent.getStudents();
+		
+		assertEquals(expected, resultStudents);
+	}
+	
+	@Test
+	void testAddStudentToEvent_WrongEventID_BadCase(){
+		int eventID = 3;
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(2);
+		
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(student, eventID);
+		});
+	}
+	
+	@Test
+	void testAddStudentToEvent_TooManyStudents_BadCase() throws StudyUpException {
+		int eventID = 1;
+		Student student1 = new Student();
+		Student student2 = new Student();
+		Student student3 = new Student();
+		student1.setFirstName("Jane");
+		student2.setFirstName("Frodo");
+		student3.setFirstName("Samwise");
+		student1.setLastName("Doe");
+		student2.setLastName("Baggins");
+		student3.setLastName("Gamgee");
+		student1.setEmail("JaneDoe@email.com");
+		student2.setEmail("FrodoBaggins@email.com");
+		student3.setEmail("SamwiseGamgee@email.com");
+		student1.setId(1);
+		student2.setId(2);
+		student3.setId(3);
+		
+		List<Student> eventStudents = new ArrayList<>();
+		eventStudents.add(student1);
+		eventStudents.add(student2);
+		
+		DataStorage.eventData.get(eventID).setStudents(eventStudents);
+
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(student3, eventID);
+		});
+	}
+	
+	@Test
+	void testDeleteEvent_GoodCase() throws StudyUpException {
+		int eventID = 1;
+		
+		Event expected = DataStorage.eventData.get(eventID);
+		
+		Event result = eventServiceImpl.deleteEvent(eventID);
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	void testDeleteEvent_WrongEventID_BadCase() throws StudyUpException {
+		int eventID = 3;
+		
+		Event result = eventServiceImpl.deleteEvent(eventID);
+		assertEquals(null, result);
+	}
 }
