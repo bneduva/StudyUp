ab589b0e0235f310bdca569bf6f98c8d00c29138
Bilal Pervaiz <bspervaiz@ucdavis.edu>
2019-02-11 23:47:14 -0800

    pushing servicesImpl files + others

--- src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
+++ src/main/java/edu/studyup/serviceImpl/EventServiceImpl.java
@@ -1,74 +1,158 @@
 package edu.studyup.serviceImpl;
 
+
+
 import java.util.ArrayList;
+
 import java.util.Date;
+
 import java.util.List;
+
 import java.util.Map;
 
+
+
 import edu.studyup.entity.Event;
+
 import edu.studyup.entity.Student;
+
 import edu.studyup.service.EventService;
+
 import edu.studyup.util.DataStorage;
+
 import edu.studyup.util.StudyUpException;
+
+
 
 public class EventServiceImpl implements EventService {
 
-	@Override
-	public Event updateEvent(Event event) {
-		DataStorage.eventData.put(event.getEventID(), event);
-		event = DataStorage.eventData.get(event.getEventID());
-		return event;
-	}
 
-	@Override
-	public List<Event> getActiveEvents() {
-		Map<Integer, Event> eventData = DataStorage.eventData;
-		List<Event> activeEvents = new ArrayList<>();
-		
-		// Checks if an event date is before today, if no, then add to the active event list.
-		for (int i=0;i<eventData.size();i++) {
-			Event ithEvent= eventData.get(i);
-			if(!ithEvent.getDate().before(new Date())) {
-				activeEvents.add(ithEvent);
-			}
-		}
-		return activeEvents;
-	}
 
+	
 	@Override
-	public List<Event> getPastEvents() {
-		Map<Integer, Event> eventData = DataStorage.eventData;
-		List<Event> pastEvents = new ArrayList<>();
-		
-		// Checks if an event date is before today, if no, then add to the active event list.
-		for (int i=0;i<eventData.size();i++) {
-			Event ithEvent= eventData.get(i);
-			if(ithEvent.getDate().before(new Date())) {
-				pastEvents.add(ithEvent);
-			}
-		}
-		return pastEvents;
-	}
 
-	@Override
-	public boolean addStudentToEvent(Student student, int eventID) throws Exception {
+	public Event updateEventName(int eventID, String name) throws StudyUpException {
+
 		Event event = DataStorage.eventData.get(eventID);
+
 		if(event == null) {
+
 			throw new StudyUpException("No event found.");
+
 		}
-		List<Student> presentStudents = event.getStudents();
-		//Todo Check
-		presentStudents.add(student);
-		event.setStudents(presentStudents);
+
+
+
+		if(name.length() >= 20) {
+
+			throw new StudyUpException("Length too long. Maximun is 20");
+
+		}
+
+		event.setName(name);
+
 		DataStorage.eventData.put(eventID, event);
-		return false;
+
+		event = DataStorage.eventData.get(event.getEventID());
+
+		return event;
+
 	}
 
+
+
 	@Override
-	public boolean deleteEvent(int eventID) {
-		DataStorage.eventData.remove(eventID);
-		//Todo error
-		return false;
+
+	public List<Event> getActiveEvents() {
+
+		Map<Integer, Event> eventData = DataStorage.eventData;
+
+		List<Event> activeEvents = new ArrayList<>();
+
+		
+
+		for (Integer key : eventData.keySet()) {
+
+			Event ithEvent= eventData.get(key);
+
+			activeEvents.add(ithEvent);
+
+		}
+
+		return activeEvents;
+
 	}
+
+
+
+	@Override
+
+	public List<Event> getPastEvents() {
+
+		Map<Integer, Event> eventData = DataStorage.eventData;
+
+		List<Event> pastEvents = new ArrayList<>();
+
+		
+
+		for (Integer key : eventData.keySet()) {
+
+			Event ithEvent= eventData.get(key);
+
+			// Checks if an event date is before today, if yes, then add to the past event list.
+
+			if(ithEvent.getDate().before(new Date())) {
+
+				pastEvents.add(ithEvent);
+
+			}
+
+		}
+
+		return pastEvents;
+
+	}
+
+
+
+	@Override
+
+	public Event addStudentToEvent(Student student, int eventID) throws StudyUpException {
+
+		Event event = DataStorage.eventData.get(eventID);
+
+		if(event == null) {
+
+			throw new StudyUpException("No event found.");
+
+		}
+
+		List<Student> presentStudents = event.getStudents();
+
+		if(presentStudents == null) {
+
+			presentStudents = new ArrayList<>();
+
+		}
+
+		presentStudents.add(student);
+
+		event.setStudents(presentStudents);		
+
+		return DataStorage.eventData.put(eventID, event);
+
+	}
+
+
+
+	@Override
+
+	public Event deleteEvent(int eventID) {		
+
+		return DataStorage.eventData.remove(eventID);
+
+	}
+
+
 
 }
