f29fe51cb9810da657519dc2e12c44c6bcc62903
Ben Neduva <ben.neduva@gmail.com>
2019-03-14 06:13:47 +0000

    Added parameter check to hotswap.sh (EC)

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -2,5 +2,9 @@
 NEWDB=$1
 COMM="/usr/sbin/nginx -s reload"
 
-sed -i "s/server\s.*\:/server $NEWDB:/g" /etc/nginx/nginx.conf
-eval $COMM
+if grep -q "$NEWDB:6379" /etc/nginx/nginx.conf; then
+    echo "Already connected to $NEWDB!"
+else
+    sed -i "s/server\s.*\:/server $NEWDB:/g" /etc/nginx/nginx.conf
+	eval $COMM
+fi
